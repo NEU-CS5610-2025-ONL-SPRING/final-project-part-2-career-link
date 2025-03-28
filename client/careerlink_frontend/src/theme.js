e16bdca53f1892b047fdeefc0e5cea3a7761b6d5
SandeshJIT <hssandesh111@gmail.com>
2025-03-27 22:59:11 -0400

    modify theme

--- client/careerlink_frontend/src/theme.js
+++ client/careerlink_frontend/src/theme.js
@@ -3,7 +3,7 @@ import { createTheme } from "@mui/material/styles";
 const theme = createTheme({
   palette: {
     primary: {
-      main: "#1976d2", // Replace with your preferred primary color
+      main: "#1976d2",
       dark: "#1565c0",
       contrastText: "#ffffff",
     },
