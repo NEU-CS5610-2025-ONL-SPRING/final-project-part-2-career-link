0daeb1ad9c60ae139977a507c057f03e38bea390
SandeshJIT <hssandesh111@gmail.com>
2025-04-20 02:34:27 -0400

    Allow multiple origin

--- api/index.js
+++ api/index.js
@@ -20,9 +20,25 @@ const {
 } = require("./authMiddleWare");
 require("dotenv").config();
 
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://careerlink-frontend-p5v1a9x0v-sandeshs-projects-cc7c8af1.vercel.app",
+];
+
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: "*", credentials: true }));
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
