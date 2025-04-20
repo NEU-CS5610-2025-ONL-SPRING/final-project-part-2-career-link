417602e66e6d7fc9833f89b73071faacb655a1fc
SandeshJIT <hssandesh111@gmail.com>
2025-04-20 02:16:26 -0400

    Change cros settings

--- api/index.js
+++ api/index.js
@@ -22,7 +22,7 @@ require("dotenv").config();
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({ origin: "*", credentials: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
